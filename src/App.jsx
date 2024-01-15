e35cb5c05e008f629ad13c6706e215f825f1745c
shubhanshurav <shubhanshurao7037@gmail.com>
2024-01-16 02:51:10 +0530

    Add Ed-Tech Code

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   }, [])
 
   return (
-    <div className="flex min-h-screen w-screen flex-col bg-pure-greys-400 font-inter">
+    <div className="flex min-h-screen w-screen flex-col bg-richblack-900 font-inter">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
