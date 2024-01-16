c11e6472f7207443d230e26abeb001ceac4db5ca
shubhanshurav <shubhanshurao7037@gmail.com>
2024-01-16 17:39:11 +0530

    Update code

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -64,7 +64,7 @@ function Navbar() {
         location.pathname !== "/" ? "bg-richblack-800" : ""
       } transition-all duration-200`}
     >
-      <div className="flex w-11/12 max-w-maxContent items-center justify-between py-4">
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link to="/">
           {/* <img src={logo} alt="Logo" width={160} height={32} loading="lazy" /> */}
