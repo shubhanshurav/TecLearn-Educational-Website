54b88e368181899f6f3d665c8c620e20c3eeaed7
shubhanshurav <shubhanshurao7037@gmail.com>
2024-01-22 02:27:09 +0530

    modify code

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ const dotenv = require("dotenv");
 
 // Setting up port number
 const PORT = process.env.PORT || 4000;
-const allowedOrigins = ['http://localhost:3000', process.env.FRONTEND_URL];
 
 // Loading environment variables from .env file
 dotenv.config();
@@ -26,29 +25,16 @@ database.connect();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-// 	// cors({
-// 	// 	origin: "http://localhost:3000",
-// 	// 	credentials: true,
-// 	// })
-// 	cors({
-// 		origin: process.env.FRONTEND_URL,
-// 		credentials: true, 
-// 	})
-// );
-
 app.use(
+	// cors({
+	// 	origin: "http://localhost:3000",
+	// 	credentials: true,
+	// })
 	cors({
-	  origin: function (origin, callback) {
-		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-		  callback(null, true);
-		} else {
-		  callback(new Error('Not allowed by CORS'));
-		}
-	  },
-	  credentials: true,
+		origin: process.env.FRONTEND_URL || "http://localhost:3000",
+		credentials: true, 
 	})
-  );
+);
 
 app.use(
 	fileUpload({
