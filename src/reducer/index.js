c02ea1dfc218c0f32122660ad37b2facbff6f097
shubhanshurav <shubhanshurao7037@gmail.com>
2024-02-12 02:07:34 +0530

    update the code

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -6,9 +6,9 @@ import courseReducer from "../slices/courseSlice"
 import profileReducer from "../slices/profileSlice"
 import viewCourseReducer from "../slices/viewCourseSlice"
 
-const rootReducer = combineReducers({
+const rootReducer = combineReducers({ 
   auth: authReducer,
-  profile: profileReducer,
+  profile: profileReducer, 
   course: courseReducer,
   cart: cartReducer,
   viewCourse: viewCourseReducer,
